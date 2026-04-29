0454e5afd88a2dcb2528935b3e8edce373b2cc04
ayano <ayano.hokulea@gmail.com>
2026-04-29 17:43:19 +1000

    Fix Card and image style to ensure image size consistency and fully cover space

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -6,8 +6,8 @@ type cardProps = {
 
 export default function Card({imgUrl, imgAlt, cardText=""}: cardProps) {
     return (
-        <div className="w-full p-4 rounded-lg shadow-md shadow-taupe-200 mb-5">
-            <img src={imgUrl} alt={imgAlt} className="rounded-lg" />
+        <div className="w-full aspect-[4/3] p-4 rounded-lg shadow-md shadow-taupe-200 mb-5">
+            <img src={imgUrl} alt={imgAlt} className="rounded-lg w-full h-full object-cover" />
             {cardText !== "" ? <p>{cardText}</p> : null}
         </div>
     );
