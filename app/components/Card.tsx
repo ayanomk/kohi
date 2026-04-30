41c0f1ca773c35213cec20389f7674442a705147
ayano <ayano.hokulea@gmail.com>
2026-04-30 16:11:54 +1000

    Fix card image and menu display for mobile

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -8,8 +8,8 @@ export default function Card({imgUrl, imgAlt, cardText=""}: cardProps) {
     const hasText = cardText?.trim();
     return (
         <div className="w-full aspect-[5/3] p-4 rounded-lg shadow-md shadow-taupe-200 mb-5 flex md:flex-col">
-            <img src={imgUrl} alt={imgAlt} className={`rounded-lg h-full object-cover ${hasText ? "w-3/5 md:w-full" : "w-full"}`} />
-            {hasText ? <div className="w-2/5 md:w-full flex flex-col md:flex-row justify-center items-center text-center p-5"><p>{cardText}</p></div> : null}
+            <img src={imgUrl} alt={imgAlt} className={`rounded-lg h-full object-cover ${hasText ? "w-2/3 md:w-full" : "w-full"}`} />
+            {hasText ? <div className="w-1/3 md:w-full flex flex-col md:flex-row justify-center items-center text-center p-5"><p>{cardText}</p></div> : null}
         </div>
     );
 }
