1e23b94609c28b5933a0ec7498d5313e3ea539dc
ayano <ayano.hokulea@gmail.com>
2026-04-30 15:31:46 +1000

    Adjust text style of Card

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -9,7 +9,7 @@ export default function Card({imgUrl, imgAlt, cardText=""}: cardProps) {
     return (
         <div className="w-full aspect-[5/3] p-4 rounded-lg shadow-md shadow-taupe-200 mb-5 flex">
             <img src={imgUrl} alt={imgAlt} className={`rounded-lg h-full object-cover ${hasText ? "w-2/3" : "w-full"}`} />
-            {hasText ? <p className="flex justify-center items-center p-5">{cardText}</p> : null}
+            {hasText ? <div className="w-1/3 flex flex-col justify-center items-center text-center p-5">{cardText.split(",").map((text, idx) => <p key={idx}>{text}</p>)}</div> : null}
         </div>
     );
 }
