5c84cc20d16cdebc868bbb8e6b9bd884788a4d21
ayano <ayano.hokulea@gmail.com>
2026-04-30 13:39:21 +1000

    Adjust Footer style for tablet and laptop

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,14 +1,14 @@
 export default function Footer() {
     return (
         <footer className="bg-secondary ">
-            <div className="flex justify-between p-5">
+            <div className="flex justify-between p-5 md:w-[90%] lg:w-[80%] mx-auto">
                 <div className="w-1/2">
-                    <img src="/logo_transparent.png" alt="logo of cafe kohi" className="w-2/3 mb-3 rounded-md" />
+                    <img src="/logo_transparent.png" alt="logo of cafe kohi" className="w-full max-w-[120px] mb-3 rounded-md" />
                     <p>TEL: 0450 123 456</p>
                 </div>
                 <div className="flex flex-row self-end w-1/4 justify-end">
-                    <img src="/instagram-stroke-rounded.svg" className="w-1/3" />
-                    <img src="/facebook-01-stroke-rounded.svg" className="w-1/3" />
+                    <img src="/instagram-stroke-rounded.svg" className="w-full max-w-[40px]" />
+                    <img src="/facebook-01-stroke-rounded.svg" className="w-full max-w-[40px]" />
                 </div>
             </div>
             <p className="text-center mb-2">&copy; 2026 KōHi. All rights reserved.</p>
