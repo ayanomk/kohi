c4f62e96d4d330fdf2ce21cbed42a5adedf17588
ayano <ayano.hokulea@gmail.com>
2026-04-30 11:22:36 +1000

    Ensure handle undefined cardText

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -8,7 +8,7 @@ export default function Card({imgUrl, imgAlt, cardText=""}: cardProps) {
     return (
         <div className="w-full aspect-[4/3] p-4 rounded-lg shadow-md shadow-taupe-200 mb-5">
             <img src={imgUrl} alt={imgAlt} className="rounded-lg w-full h-full object-cover" />
-            {cardText !== "" ? <p>{cardText}</p> : null}
+            {cardText?.trim() ? <p>{cardText}</p> : null}
         </div>
     );
 }
